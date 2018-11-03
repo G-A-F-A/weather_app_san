aeb15c37336702f03fb666f5c1375c5d6688f763
Hirota <hirotake.dadada@gmail.com>
2018-11-03 17:31:32 +0900

    [Add] Apiをキックする処理を追加

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,7 +1,22 @@
 class WeatherController < ApplicationController
+  before_action :set_connection
+
   def index
+    @res = @connection.get do |req|
+      req.url '/data/2.5/weather'
+      req.params[:q] = 'shibuya,jp'
+      req.params[:appid] = ENV['WEATHER_API_KEY']
+      req.params[:units] = 'metric'
+    end
+    binding.pry
   end
 
   def show
   end
+
+  private
+
+    def set_connection
+      @connection = Faraday.new(url: ENV['WEATHER_API_URL'])
+    end
 end
